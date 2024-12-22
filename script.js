33b7ed2ef5683e681ce611adb1d7f1b553741ac2
Jack-Omnium-Artium <152470190+Jack-Omnium-Artium@users.noreply.github.com>
2024-12-22 00:22:12 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const header = document.getElementsByClassName('.header');
+
+const button = document.getElementById('Inicio');
+
+const tarjeta = document.querySelectorAll('.tarjeta');
+
+const footer = document.querySelector("footer");
+
+
+
+
+
+
+
